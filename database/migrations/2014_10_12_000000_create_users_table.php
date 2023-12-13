c2a44b2996c8aa2d2cd8c1705935a0b311f3394d
Mostafa Alaeddin <mostafaalaeddin570@gmail.com>
2023-12-13 11:10:15 +0330

    fixed users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('address');
+            $table->string('cellphone');
+            $table->unsignedInteger('province_id');
+            $table->unsignedInteger('city_id');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
