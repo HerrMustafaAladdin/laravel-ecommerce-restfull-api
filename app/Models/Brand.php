fc8f17ca795b342bd74e3114684c63861b55c242
Mostafa Alaeddin <mostafaalaeddin570@gmail.com>
2023-12-13 09:51:59 +0330

    create model Brand

---
+++ app/Models/Brand.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Brand extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * @var string
+     */
+    protected $table = "brands";
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'display_name'
+    ];
+}
