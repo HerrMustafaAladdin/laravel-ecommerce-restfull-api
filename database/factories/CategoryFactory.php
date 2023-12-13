e7a5513eb174d1f67628042a5313c4139fa0bfe0
Mostafa Alaeddin <mostafaalaeddin570@gmail.com>
2023-12-13 10:01:47 +0330

    fixed category factory

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Brand;
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<\App\Models\Model>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'parent_id'     =>  Category::query()->where('id', '<>', $this->id)->pluck('id')->random()->first(),
+            'name'          =>  $this->faker->name,
+            'description'   =>  $this->faker->text
+        ];
+    }
+}
