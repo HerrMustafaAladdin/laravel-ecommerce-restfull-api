5f0a595cbe968aeb4d3d8477a8ea68764ddfd76f
Mostafa Alaeddin <mostafaalaeddin570@gmail.com>
2023-12-13 10:10:40 +0330

    added products migration

---
+++ database/migrations/2023_12_13_063223_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('brand_id');
+            $table->foreign('brand_id')->references('id')->on('brands')->cascadeOnDelete()->cascadeOnUpdate();
+
+            $table->foreignId('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->cascadeOnUpdate()->cascadeOnDelete();
+
+            $table->string('primary_image');
+            $table->string('price');
+            $table->string('quantity');
+            $table->text('description');
+            $table->string('delivery_amount');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
