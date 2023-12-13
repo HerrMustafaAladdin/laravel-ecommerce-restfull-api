4565e2c65d87dbd73070af46c5a9bf3baf6aa1e1
Mostafa Alaeddin <mostafaalaeddin570@gmail.com>
2023-12-13 10:10:25 +0330

    added model products

---
+++ app/Models/Product.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    /**
+     * @var string
+     */
+    protected $table = "products";
+
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'brand_id',
+        'category_id',
+        'primary_image',
+        'price',
+        'quantity',
+        'description',
+        'delivery_amount'
+    ];
+
+}
