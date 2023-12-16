623f18111685ed0ed8343dbfe33c875fbfd8bec0
Mostafa Alaeddin <mostafaalaeddin570@gmail.com>
2023-12-16 11:03:35 +0330

    fixed exception errors

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,8 +3,13 @@
 namespace App\Exceptions;
 
 use App\Traits\ApiResponser;
+use Error;
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -33,6 +38,36 @@ class Handler extends ExceptionHandler
 
     public function render($request, Throwable $e)
     {
-        
+        if($e instanceof ModelNotFoundException)
+        {
+            return $this->errorResponce($e->getMessage(), 404);
+        }
+
+        if($e instanceof NotFoundHttpException)
+        {
+            return $this->errorResponce($e->getMessage(), 404);
+        }
+
+        if($e instanceof MethodNotAllowedHttpException)
+        {
+            return $this->errorResponce($e->getMessage(), 404);
+        }
+
+        if($e instanceof Exception)
+        {
+            return $this->errorResponce($e->getMessage(), 404);
+        }
+
+        if($e instanceof Error)
+        {
+            return $this->errorResponce($e->getMessage(), 404);
+        }
+
+        if(config('app.debug'))
+        {
+            return parent::render($request, $e);
+        }
+
+        return $this->errorResponce($e->getMessage(), 500);
     }
 }
