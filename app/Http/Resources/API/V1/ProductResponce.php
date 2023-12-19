b770d1f550518059653da20844078ea09bf81c45
Mostafa Alaeddin <mostafaalaeddin570@gmail.com>
2023-12-19 10:49:06 +0330

    fixed peoduct responce

--- app/Http/Resources/API/V1/ProductResponce.php
+++ app/Http/Resources/API/V1/ProductResponce.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources\API\V1;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
-
+use App\Http\Resources\API\V1\ProductImnageResponce;
 class ProductResponce extends JsonResource
 {
     /**
@@ -15,13 +15,15 @@ class ProductResponce extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            "Brand"             =>  $this->brand_id,
-            "Category"          =>  $this->category_id,
+            "primary_image"     =>  asset(env('PRODUCT_SHOW_PATH_IMAGES') . $this->primary_image),
+            "Brand"             =>  $this->brand->name,
+            "Category"          =>  $this->category->name,
             "price"             =>  $this->price,
             "quantity"          =>  $this->quantity,
             "description"       =>  $this->description,
             "delivery_amount"   =>  $this->delivery_amount,
             "created_at"        =>  $this->created_at,
+            "Images"            =>  ProductImnageResponce::collection($this->whenLoaded('images')),
         ];
     }
 }
