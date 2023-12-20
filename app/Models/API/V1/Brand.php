01b88d88394b9ebe3255424d5c585e3a00adb4b5
Mostafa Alaeddin <mostafaalaeddin570@gmail.com>
2023-12-20 08:29:27 +0330

    fixed brand model

--- app/Models/API/V1/Brand.php
+++ app/Models/API/V1/Brand.php
@@ -24,4 +24,11 @@ class Brand extends Model
         'created_at',
         'updated_at'
     ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+
 }
